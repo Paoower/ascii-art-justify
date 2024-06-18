3d7c7403c1a81c3c786089ec2e0c0ff304f91b14
mtrebert <maxime.trebert@gmail.com>
2024-06-18 16:40:55 +0200

    not ascii character

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module ascii-art-justify
 
-go 1.23
+go 1.22.4
--- main.go
+++ main.go
@@ -14,11 +14,9 @@ func main() {
 		fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nExample: go run . --align=right something standard")
 		return
 	}
-
 	var input, style, align string
 	style = "standard"
 	align = "left"
-
 	argIndex := 1
 	for argIndex < len(os.Args) {
 		arg := os.Args[argIndex]
@@ -39,87 +37,86 @@ func main() {
 		}
 		argIndex++
 	}
-
 	if input == "" {
 		fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nExample: go run . --align=right something standard")
 		return
 	}
-
 	// Use correct Banner files
 	bannerFile, err := src.GetBannerFile(style)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-
 	lines := make([]string, 0)
 	words := strings.Split(input, "\\n")
-
 	if align == "justify" {
 		numOfSpaces := strings.Count(input, " ")
-
 		for _, word := range words {
 			if word == "" {
 				lines = append(lines, "")
 			} else {
-				lines = append(lines, src.GetWord(Trim(word), bannerFile)...)
+				getW, err := src.GetWord(word, bannerFile)
+				if err != nil {
+					fmt.Println(err)
+				}
+				lines = append(lines, getW...)
 			}
 		}
-
 		lenOfOneLine := len(lines[0])
 		spacesToRemain := getTerminalWidth() - lenOfOneLine
-
 		lines = []string{}
 		lines = append(lines, src.GetJustifiedAscii(input, style, spacesToRemain, numOfSpaces)...)
 	}
-
 	if align == "center" {
 		for _, word := range words {
 			if word == "" {
 				lines = append(lines, "")
 			} else {
-				lines = append(lines, src.GetWord(word, bannerFile)...)
+				getW, err := src.GetWord(word, bannerFile)
+				if err != nil {
+					fmt.Println(err)
+				}
+				lines = append(lines, getW...)
 			}
 		}
-
 		lenOfOneLine := len(lines[0])
 		spacesToRemain := getTerminalWidth() - lenOfOneLine
-
 		lines = []string{}
 		lines = append(lines, src.GetCenteredAscii(input, style, spacesToRemain, 2)...)
 	}
-
 	if align == "right" {
 		for _, word := range words {
 			if word == "" {
 				lines = append(lines, "")
 			} else {
-				lines = append(lines, src.GetWord(word, bannerFile)...)
+				getW, err := src.GetWord(word, bannerFile)
+				if err != nil {
+					fmt.Println(err)
+				}
+				lines = append(lines, getW...)
 			}
 		}
-
 		lenOfOneLine := len(lines[0])
 		spacesToRemain := getTerminalWidth() - lenOfOneLine
-
 		lines = []string{}
-
 		lines = append(lines, src.GetCenteredAscii(input, style, spacesToRemain, 1)...)
 	}
-
 	if align == "left" {
 		for _, word := range words {
 			if word == "" {
 				lines = append(lines, "")
 			} else {
-				lines = append(lines, src.GetWord(word, bannerFile)...)
+				getW, err := src.GetWord(word, bannerFile)
+				if err != nil {
+					fmt.Println(err)
+				}
+				lines = append(lines, getW...)
 			}
 		}
 	}
-
 	for _, line := range lines {
 		fmt.Println(line)
 	}
-
 }
 
 // Get the width of the terminal
@@ -136,7 +133,6 @@ func getTerminalWidth() int {
 	}
 	return int(dimensions[1])
 }
-
 func Trim(s string) string {
 	str := ""
 	for _, char := range s {
